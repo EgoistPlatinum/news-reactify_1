6f6226e36b27071a5923fac1ed9e29492b3c6b59
Aleksey Leonov <74103840+AlekseyLeonov031@users.noreply.github.com>
2025-02-03 16:36:03 +0500

    feature/NR-8 (Add latest news API integration and refactor components
                  Introduced `getLatestNews` API method and updated `LatestNews` component to fetch data dynamically. Removed obsolete props in `Main` and `NewsByFilters` components, refactored logic into hooks, and introduced `PaginationWrapper` for reusable pagination controls. Added scroll styling to `BannersList`.)

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -1,11 +1,17 @@
 import styles from './styles.module.css'
 import BannersList from "../BannersList/BannersList.jsx";
+import {useFetch} from "../../helpers/hooks/useFetch.js";
+import {getLatestNews} from "../../api/apiNews.js";
+import PaginationWrapper from "../PaginationWrapper/PaginationWrapper.jsx";
+import {TOTAL_PAGES} from "../constants/constants.js";
 
-const LatestNews = ({banners, isLoading}) => {
+const LatestNews = () => {
+
+  const {data, isLoading} = useFetch(getLatestNews)
 
   return (
     <section className={styles.section}>
-      <BannersList banners={banners} isLoading={isLoading}/>
+      <BannersList banners={data && data.news} isLoading={isLoading}/>
     </section>
   )
 }
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,34 +1,13 @@
 import styles from "./styles.module.css"
-import {getNews} from "../../api/apiNews.js";
-import useDebounce from "../../helpers/hooks/useDebounce.jsx";
-import {PAGE_SIZE} from "../../components/constants/constants.js";
-import {useFetch} from "../../helpers/hooks/useFetch.js";
-import {useFilters} from "../../helpers/hooks/useFilters.js";
 import LatestNews from "../../components/LatestNews/LatestNews.jsx";
 import NewsByFilters from "../../components/NewsByFilters/NewsByFilters.jsx";
 
 const Main = () => {
 
-  const {filters, changeFilters} = useFilters({
-    page_size: PAGE_SIZE,
-    page_number: 1,
-    category: null,
-    keywords: ''
-  })
-
-  const debounceKeywords = useDebounce(filters.keywords, 1500)
-
-  const {data, isLoading} = useFetch(getNews, {
-    ...filters,
-    keywords: debounceKeywords
-  })
-
-
   return (
     <main className={styles.main}>
-      <LatestNews banners={data && data.news} isLoading={isLoading}/>
-
-      <NewsByFilters news={data?.news} changeFilters={changeFilters} isLoading={isLoading} filters={filters}/>
+      <LatestNews/>
+      <NewsByFilters/>
     </main>
   )
 }
